3cceb5cea3d13fc4deab9038807f3167bbe15011
Feud-Rose <145285546+Feud-Rose@users.noreply.github.com>
2024-01-10 14:41:56 -0700

    Add forecast

--- src/modules/displayWeather.js
+++ src/modules/displayWeather.js
@@ -18,27 +18,39 @@ function updateToday(weatherData){
     console.log(locationHeader)
     locationHeader.textContent = `Weather in ${weatherData.location.name}, ${weatherData.location.region}, ${weatherData.location.country}.` 
 
-    let conditionDiv = document.createElement('div')
-    conditionDiv.classList.add('conditions')
-    todayDiv.appendChild(conditionDiv)
-    conditionDiv.textContent = condition.day
-    
-    let icon = document.createElement("img")
-    icon.src = `./images/day/${condition.icon}.png`
-    conditionDiv.appendChild(icon)
+    let currentDescription = document.querySelector(".currentDescription")
+    console.log(currentDescription)
+    currentDescription.textContent = condition.day
 
-    let currentTemp = document.createElement('div')
-    currentTemp.textContent = `Current Temperature: ${weatherData.current.temp_c}, Feels like: ${weatherData.current.feelslike_c} `
-    todayDiv.appendChild(currentTemp)
+    let img = document.querySelector(".todayIcon")
+    img.src = `./images/day/${condition.icon}.png`
 
+    let currentTemp = document.querySelector('.currentTemp')
+    currentTemp.textContent = `${weatherData.current.temp_c}\u00B0C`;
+   
+
+     let feelsLike = document.querySelector('.feelsLike')
+    feelsLike.textContent = `${weatherData.current.feelslike_c}\u00B0C `;
 }
 
 function updateForecast(weatherData, day) {
-console.log(weatherData)
+
 const div = document.querySelector(day)
+console.log(div)
 let date = new Date(weatherData.date).toLocaleDateString('en-us', { weekday:"long"})
 console.log(date)
 div.firstChild.textContent = date
+
+const condition = processCode(weatherData.day.condition.code)
+
+let img = document.querySelector(`${day}Icon`)
+img.src = `./images/day/${condition.icon}.png`
+img.alt = weatherData.day.condition.text
+img.title = weatherData.day.condition.text
+
+let temp = document.querySelector(`${day}Temp`)
+temp.textContent = `${weatherData.day.maxtemp_c}\u00B0C ${weatherData.day.mintemp_c}\u00B0C`;
+
 }
 
 function processCode(num) {
